74d41cbfd0a6bb03ac1a47b6afcbcce570935d76
Jmacro <jonmacrohon@gmail.com>
2020-02-18 15:21:00 +1100

    Updates

---
+++ precache-manifest.a148244c35d4706956524505b0f30a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81babd8050833adf5a0c0186cda9d2d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ba423b95d258c70a5c66",
+    "url": "/react-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7dfddd13d832520f6cef",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba423b95d258c70a5c66",
+    "url": "/react-app/static/js/2.f4c1be1d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app/static/js/2.f4c1be1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfddd13d832520f6cef",
+    "url": "/react-app/static/js/main.280a6095.chunk.js"
+  },
+  {
+    "revision": "0fd96d369b62d4317ce7",
+    "url": "/react-app/static/js/runtime-main.b06c7335.js"
+  }
+]);
